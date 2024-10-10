82d5f78159a91fa8950b9227dc38bcc3e2ae4a1a
Taufik <taufik9890@gmail.com>
2024-10-10 18:31:58 +0600

    image url fiz

--- frontend/components/ProCard.js
+++ frontend/components/ProCard.js
@@ -19,7 +19,7 @@ const ProCard = ({data}) => {
     data.map((item)=>(
         <Card style={{ width: '18rem' }}>
       <Image
-        src={`https://127.0.0.1:8000${item.image}`}
+        src={`http://127.0.0.1:8000${item.image}`}
         width={500}
         height={500}
         alt="Picture of the author"
