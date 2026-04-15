8cbec798ca98c992acf0a98c5b8a7e6bc01ea368
Taufik Mahbub <77428168+taufik9890@users.noreply.github.com>
2026-04-16 04:24:28 +0600

    new2

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -71,7 +71,7 @@ let registrationController = async (req, res) => {
     const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT),
-  secure: true, 
+  secure: false, 
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
